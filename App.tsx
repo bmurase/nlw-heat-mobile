fb9a99d33373380f382e93f2c9b54b7043bd9ba9
bmurase <biamurase@gmail.com>
2021-10-23 21:34:12 -0300

    :lipstick: add fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
 import { Home } from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Home />
   );
