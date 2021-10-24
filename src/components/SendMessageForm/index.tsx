0301832de09bbfd011ea489706f0a0e1bb097d36
bmurase <biamurase@gmail.com>
2021-10-24 01:56:19 -0300

    :rocket: send message

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Alert, Keyboard, Text, TextInput, View } from 'react-native';
+import { api } from '../../services/api';
 import { COLORS } from '../../theme';
 import { Button } from '../Button';
 
@@ -8,6 +9,22 @@ import { styles } from './styles';
 export function SendMessageForm() {
   const [message, setMessage] = useState('');
   const [sendingMessage, setSendingMessage] = useState(false);
+
+  async function handleSendMessage() {
+    if (!message.trim()) {
+      Alert.alert('Escreva a mensagem para enviar.');
+      return;
+    }
+
+    setSendingMessage(true);
+
+    await api.post('messages', { text: message });
+
+    setMessage('');
+    Keyboard.dismiss();
+    setSendingMessage(false);
+    Alert.alert('Mensagem enviada com sucesso!');
+  }
   return (
     <View style={styles.container}>
       <TextInput
@@ -22,7 +39,13 @@ export function SendMessageForm() {
         editable={!sendingMessage}
       />
 
-      <Button title='ENVIAR MENSAGEM' color={COLORS.WHITE} backgroundColor={COLORS.PINK} />
+      <Button 
+        title='ENVIAR MENSAGEM' 
+        color={COLORS.WHITE} 
+        backgroundColor={COLORS.PINK}
+        isLoading={sendingMessage}
+        onPress={handleSendMessage}
+      />
     </View>
   )
 }
