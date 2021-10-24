1704fe33204ec2bebf22569a2bfc3aa976e359c9
bmurase <biamurase@gmail.com>
2021-10-24 01:48:31 -0300

    :rocket: list messages

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
+import { api } from '../../services/api';
 
-import { Message } from '../Message';
+import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
 export function MessageList() {
+  const [messages, setMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    api.get<MessageProps[]>('messages').then(response => {
+      setMessages(response.data);
+    });    
+  }, [])
+  
   return (
     <ScrollView 
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps='never'
     >
-      <Message data={{
-        id: '123',
-        text: 'Texto da mensagem, texto da mensagem, texto da mensagem, texto da mensagem, texto da mensagem',
-        user: {
-          name: 'Mary Jane',
-          avatar_url: 'https://randomuser.me/api/portraits/women/44.jpg'
-        }
-      }} />
-      <Message data={{
-        id: '123',
-        text: 'Texto da mensagem, texto da mensagem, texto da mensagem, texto da mensagem, texto da mensagem',
-        user: {
-          name: 'Mary Jane',
-          avatar_url: 'https://randomuser.me/api/portraits/women/44.jpg'
-        }
-      }} />
-      <Message data={{
-        id: '123',
-        text: 'Texto da mensagem, texto da mensagem, texto da mensagem, texto da mensagem, texto da mensagem',
-        user: {
-          name: 'Mary Jane',
-          avatar_url: 'https://randomuser.me/api/portraits/women/44.jpg'
-        }
-      }} />
+      {messages.map(message => 
+        <Message key={message.id} data={message} />
+      )}
     </ScrollView>
   )
 }
