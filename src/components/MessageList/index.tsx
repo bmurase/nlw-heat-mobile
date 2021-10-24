abe9c77a2605746d3b27a9ba71327e2e52cbb4ca
bmurase <biamurase@gmail.com>
2021-10-24 02:08:43 -0300

    :rocket: listing real time messages with socket.io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
+import io from 'socket.io-client';
 import { api } from '../../services/api';
 
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
+
+const messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', (newMessage: MessageProps) => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList() {
   const [messages, setMessages] = useState<MessageProps[]>([]);
@@ -13,7 +21,23 @@ export function MessageList() {
     api.get<MessageProps[]>('messages').then(response => {
       setMessages(response.data);
     });    
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
   
   return (
     <ScrollView 
